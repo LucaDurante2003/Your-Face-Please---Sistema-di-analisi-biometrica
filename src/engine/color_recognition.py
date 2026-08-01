f3a2e7243565cd3e93c00874c3022334d746c90f
LucaDurante2003 <luca.d3002@gmail.com>
2026-08-01 19:08:21 +0200

    Creazione color_recognition.py

---
+++ src/engine/color_recognition.py
@@ -0,0 +1,8 @@
+"""
+Implementare modulo per il riconoscimento del colore degli occhi, della pelle e dei capelli
+tramite il ritaglio delle aree di interesse e l'applicazione dell'algoritmo K-Means
+"""
+
+"""
+Comprendere funzionamento di algoritmo di clustering geometrico K-Means
+"""
